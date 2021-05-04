e60531bc3377b37044929638805c3e520b3039ee
Norman Basham <nbasham@yahoo.com>
2021-05-04 11:11:27 -0700

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Norman Basham on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
